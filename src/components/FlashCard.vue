9e8c834f8e05c77b9cd02902a4777905b0a1ae39
Javi <maxdevcodes@gmail.com>
2020-10-03 18:47:13 +0200

    basic flip card

---
+++ src/components/FlashCard.vue
@@ -0,0 +1,54 @@
+<template>
+    <div class="card-container">
+        <div class="inner">
+            <div class="card-front">
+                <h2 class="question">Question?</h2>
+            </div>
+            <div class="card-back">
+                <p class="answer">Answer: ...</p>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.card-container {
+    color: #fff;
+    max-width: 400px;
+    height: 300px;
+    margin: 0 auto;
+    perspective: 1200px;
+}
+
+.card-container:hover .inner {
+    transform: rotateY(-180deg);
+}
+
+.inner {
+    width: 100%;
+    height: 100%;
+    position: relative;
+    transform-style: preserve-3d;
+    transition: .7s transform;
+    background: #282c34;
+    border-radius: 8px;
+    padding: 10px 25px;
+}
+
+.card-front, .card-back {
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    backface-visibility: hidden;
+}
+
+.card-back {
+    transform: rotateY(180deg);
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,16 @@
 <template>
   <div class="home">
+    <flash-card></flash-card>
   </div>
 </template>
 
 <script>
+import FlashCard from '@/components/FlashCard.vue';
 
 export default {
   name: "Home",
   components: {
+    FlashCard
   }
 };
 </script>
