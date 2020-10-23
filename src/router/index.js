2ba3348dd8f160b31a2226540c3c1526a1215f99
Javi <maxdevcodes@gmail.com>
2020-10-23 23:39:31 +0200

    added view for flashcards

--- src/router/index.js
+++ src/router/index.js
@@ -15,6 +15,12 @@ const routes = [
     // which is lazy-loaded when the route is visited.
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/About.vue")
+  },
+  {
+    path: "/cardset",
+    name: "Cardset",
+    component: () =>
+      import(/* webpackChunkName: "cardset" */ "../views/CardSet.vue")
   }
 ];
 
---
+++ src/views/CardSet.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <flash-card></flash-card>
+        <div class="deck-buttons-container">
+            <button class="btn">Previous</button>
+            <button class="btn">Next</button>
+        </div>
+    </div>
+</template>
+
+<script>
+import FlashCard from '@/components/FlashCard.vue'; 
+export default {
+    components: {
+        FlashCard
+    }
+};
+</script>
+
+<style scoped>
+</style>
